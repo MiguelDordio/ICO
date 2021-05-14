fe334b45653f24233b4a0583dfbff7831e9d22fd
MiguelDordio <miguel.dordio.oliveira@gmail.com>
2021-05-14 01:55:41 +0100

    FrontEnd com simualçao dinamica

--- icoFrontEnd/src/app/app.module.ts
+++ icoFrontEnd/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { HttpClientModule } from '@angular/common/http'
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SimulatorComponent } from './simulator/simulator.component';
 import { MapComponent } from './map/map.component';
@@ -26,6 +26,7 @@ import { ComponentNameComponent } from './component-name/component-name.componen
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- icoFrontEnd/src/app/simulator/simulator.component.ts
+++ icoFrontEnd/src/app/simulator/simulator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Loader } from '@googlemaps/js-api-loader';
 import { AlgorithmRequest } from '../models/AlgorithmRequest';
@@ -13,10 +14,18 @@ import { SimulatorApiService } from '../simulator-api.service';
 })
 export class SimulatorComponent implements OnInit {
 
+	pageTitle: string = "";
+	pageTip: string = "";
+	pageNumber: number = 0;
+
+	vehicleForm: FormGroup;
+	packagesForm: FormGroup;
+
 	apiResponse: any;
 	showMap: boolean = true;
 	map: any;
 	selectedCoordinates: Coordinate[] = new Array();
+	mapFirstClick: boolean = true;
 
 	// inputs
 	nVehicles!: number;
@@ -25,16 +34,23 @@ export class SimulatorComponent implements OnInit {
 	nDestinies!: number;
 	demand!: number;
 
-	constructor(private simulatorApi: SimulatorApiService, private route: ActivatedRoute, private router: Router) { }
+	constructor(
+		private simulatorApi: SimulatorApiService, 
+		private route: ActivatedRoute, 
+		private router: Router,
+		private fb: FormBuilder) {
+			this.vehicleForm = this.fb.group({
+				vehicles: this.fb.array([]),
+			});
+
+			this.packagesForm = this.fb.group({
+				packages: this.fb.array([]),
+			});
+		}
 
 	ngOnInit(): void {
 		console.log("Simulator Page");
-		let loader = new Loader({
-			apiKey: 'AIzaSyBs6jtKaF86w6XSBakQ8LBEgPMqx2dZVoE'
-		});
-		loader.load().then(() => {
-			this.initMap();
-		  });
+		this.changePage(false);
 	}
 
 	initMap(): void {
@@ -45,12 +61,6 @@ export class SimulatorComponent implements OnInit {
 				center: myLatlng,
 		  	}
 		);
-	  
-		const marker = new google.maps.Marker({
-			position: myLatlng,
-			map,
-			title: "Click para aumentar",
-		});
 
 		map.addListener("click", (e: any) => {
 			this.placeMarkerAndPanTo(e.latLng, map);
@@ -61,15 +71,78 @@ export class SimulatorComponent implements OnInit {
 		new google.maps.Marker({
 		  position: latLng,
 		  map: map,
+		  icon: this.mapFirstClick ? '../../assets/depot_icon.png' : '../../assets/client_icon.png'
 		});
+		if (this.mapFirstClick) {
+			this.mapFirstClick = false;
+			this.pageTip = "Indique os locais das entregas";
+		}
 		map.panTo(latLng);
 		let coordinate = new Coordinate(latLng.lat(), latLng.lng());
 		this.selectedCoordinates.push(coordinate);
 		console.log("Lat: " + latLng.lat() + "\nLon: " + latLng.lng());
 	}
 
-	changePage() {
-		this.showMap = !this.showMap;
+	changePage(goBack: boolean) {
+		if (goBack)
+			this.pageNumber -= 2;
+
+		// From step 1 -> 2 - Map
+		if (this.pageNumber == 0) {
+			this.pageNumber++;
+			this.pageTitle = "Localizações";
+			this.pageTip = "Indique o local de partida";
+
+			if (!goBack) {
+				let loader = new Loader({
+					apiKey: 'AIzaSyBs6jtKaF86w6XSBakQ8LBEgPMqx2dZVoE'
+				});
+				loader.load().then(() => {
+					this.initMap();
+				});
+			}
+		}
+		// From step 1 -> 2 - Vehicles
+		else if (this.pageNumber == 1){
+			this.showMap = !this.showMap;
+			this.pageNumber++;
+			this.pageTitle = "Veiculos";
+			this.pageTip = "Caractize os seus veiculos";
+		}
+		// From step 2 -> 3 - Packages
+		else if (this.pageNumber == 2) {
+			this.pageNumber++;
+			this.pageTitle = "Encomendas";
+			this.pageTip = "Caractize as encomendas dos seus clientes";
+		} 
+		// From step 3 -> 4 - Simulation Resume
+		else if (this.pageNumber == 3) {
+			this.pageNumber++;
+			this.pageTitle = "Resumo de simulação";
+			this.pageTip = "Confirme os dados";
+		}
+	}
+
+	addItem() {
+		if (this.pageNumber == 2)
+			this.addVehicle()
+		else if (this.pageNumber == 3)
+			this.addPackage();		
+	}
+
+	addVehicle() {
+		const vhcs = this.vehicleForm.controls.vehicles as FormArray;
+		vhcs.push(this.fb.group({
+			maxCargo: '',
+			fuelConsumption: '',
+		}));
+	}
+
+	addPackage() {
+		const pack = this.packagesForm.controls.packages as FormArray;
+		pack.push(this.fb.group({
+			weight: ''
+		}));
 	}
 
 	simulate() {
